dc364395e4b6fbe04f9dcdf9a4ac23be103521b1
andrewnationdev <21006223@uepg.br>
2026-02-12 11:49:54 -0300

    🔍️ SEO do site

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    head: {
+      title: 'Viação Paranaense - Compra de Passagens',
+      htmlAttrs: {
+        lang: 'pt-br',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
 })
