3d1e726ef2dc9945dc67257ae05cac606809b63b
andrewnationdev <21006223@uepg.br>
2026-02-23 12:39:33 -0300

    ✨ Lógica para exibir compras anteriores

--- components/Header.vue
+++ components/Header.vue
@@ -1,19 +1,27 @@
 <template>
-  <header
-    class="px-8 py-4 top-0 z-50 opacity-[90%] m-auto w-full sticky flex justify-between bg-blue-800"
-  >
+  <header class="px-8 py-4 top-0 z-50 opacity-[90%] m-auto w-full sticky flex justify-between bg-blue-800">
     <h1 class="font-bold text-xl text-teal-200">VIAÇÃO PARANAENSE</h1>
-    <a
-      aria-role="button"
-      title="Meus Bilhetes"
-      @click="window.alert('Em implementação')"
-      class="text-slate-400 hover:cursor-pointer hover:text-white"
-    >
+    <a aria-role="button" title="Meus Bilhetes" href="" @click.prevent="showPreviousTickets()"
+      class="text-slate-400 hover:cursor-pointer hover:text-white">
       <ShoppingCart />
     </a>
   </header>
 </template>
 
 <script setup>
-  import {ShoppingCart} from 'lucide-vue-next';
+import { ShoppingCart } from 'lucide-vue-next';
+
+const {
+  showBoughtTickets
+} = useBusStore();
+
+function showPreviousTickets() {
+  if (showBoughtTickets.value == false) {
+    showBoughtTickets.value = true;
+    return;
+  } else {
+    showBoughtTickets.value = false;
+    return;
+  }
+}
 </script>
