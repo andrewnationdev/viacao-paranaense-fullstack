5f9d270f7c0975c6578a92bf138f7cd292b80b77
andrewnationdev <21006223@uepg.br>
2026-02-25 15:45:15 -0300

    ✨ Arquivo de configuração para ajustar dados do site como nome da empresa, telefone e e-mail de contato e links de redes sociais

---
+++ utils/website.config.ts
@@ -0,0 +1,13 @@
+export const WEBSITE = {
+    title: "VIAÇÃO PARANAENSE",
+    contact: {
+        phone_number: '',
+        social: {
+            instagram: '',
+            whatsapp: '',
+            facebook: ''
+        },
+        email: '',
+        address: ''
+    }
+}
