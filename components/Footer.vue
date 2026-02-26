7a90a0e8fdccbfe6d0613c34a31df2d75f6338f0
andrewnationdev <21006223@uepg.br>
2026-02-26 11:22:07 -0300

    💄 Estilização da UI

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="w-full p-8 flex-col items-center bg-blue-800">
+    <div class="w-full p-8 flex flex-col items-center bg-blue-800">
         <h1 class="font-bold text-xl my-2 text-center text-teal-200">VIAÇÃO PARANAENSE</h1>
         <div class="flex-col text-white">
             <p>{{WEBSITE.contact.phone_number}}</p>
             <p>{{WEBSITE.contact.email}}</p>
         </div>
-        <div class="flex-col text-white text-center gap-4">
+        <div class="flex flex-col items-center mt-6 text-white text-center gap-2">
             <a href="https://www.gov.br/antt/pt-br" class="hover:underline">Agência Nacional de Transportes Terrestres (ANTT)</a>
             <a href="https://www.gov.br/antt/pt-br/assuntos/passageiros/viajantes/direitos-e-deveres" class="hover:underline">Direitos e Deveres dos Passageiros</a>
         </div>
